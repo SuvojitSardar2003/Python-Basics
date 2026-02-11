f72c93f39a5739f06a5c01dca0d7e4f04c3f80ef
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2026-02-11 15:55:35 +0530

    chapter3-string

--- Python/Chap3-Strings/01_intro_to_strings.py
+++ Python/Chap3-Strings/01_intro_to_strings.py
@@ -3,4 +3,4 @@ name = "Harry"
 nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
 print(nameshort)
 character1 = name[1]
-print(character1)
+print(character1.upper())
--- Python/Chap3-Strings/02_negative_slicing.py
+++ Python/Chap3-Strings/02_negative_slicing.py
@@ -8,3 +8,11 @@ print(name[1:4])
 print(name[:4]) # is same as print(name[0:4])
 print(name[1:]) # is same as print(name[1:5])
 print(name[1:5])
+
+s= "abcdefghijklmnopqrstuvwxyz"
+
+print(s[1:9:4])
+
+n= "1234567890"
+
+print(n[0:7:2])
