ecf35b7ce6b6db435b943bac1e1d42a7100a7dc8
SuvojitSardar2003 <sardarsuvojit720@gmail.com>
2026-02-16 23:38:53 +0530

    Add classes for 2-D and 3-D vectors, and implement vector display methods

---
+++ Python/Chap11-Inheritance & more on OOPs/Problem Set/Problem4.py
@@ -0,0 +1,11 @@
+#4. Write a class 'Complex' to represent complex numbers, along with overloaded operators '+' and '*' which adds and multiplies them.
+
+#5. Write a class vector representing a vector of n dimensions. Overload the + and * operator which calculates the sum and the dot(.) product of them.
+
+#6. Write_str_() method to print the vector as follows:
+
+# 71 +81 +10k
+
+#Assume vector of dimension 3 for this problem.
+
+#7. Override the _len_() method on vector of problem 5 to display the dimension of the vector.
